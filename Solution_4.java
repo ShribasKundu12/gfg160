cc5e33c16e07bc05139820b33e29db68d1cf1a9a
Shribas Kundu <147904187+ShribasKundu12@users.noreply.github.com>
2024-11-23 20:52:37 +0530

    Add files via upload

---
+++ Solution_4.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+/**
+ * User function Template for Java
+ * array reversal approach for rotating an array
+ */
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void reverse(int []arr, int start, int end){
+        int temp;
+        while(start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    static void rotateArr(int arr[], int d) {
+        //when d>n;
+        d = d % arr.length;
+        if(d==0){
+            return;
+        }
+        //for 0 to d-1;
+        reverse(arr,0,d-1);
+        //for d to last
+        reverse(arr,d,arr.length-1);
+        //entire array
+        reverse(arr,0,arr.length-1);
+
+    }
+}
