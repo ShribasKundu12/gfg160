26efb8d0e1e539a62850c481646a77f2c5fda0c6
Shribas Kundu <147904187+ShribasKundu12@users.noreply.github.com>
2025-01-19 23:25:31 +0530

    Add files via upload

---
+++ Solution_61.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(
+                    token)); // Use Integer.parseInt to parse int values
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling equilibriumPoint() function
+            System.out.println(obj.findEquilibrium(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find equilibrium point in the array.
+    public static int findEquilibrium(int arr[]) {
+        // code here
+        int presum = 0,total =0;
+        for(int ele:arr){
+            total+= ele;
+        }
+        for(int pivot = 0; pivot<arr.length; pivot++){
+            int suffsum = total-presum-arr[pivot];
+            if(presum == suffsum){
+                return pivot;
+            }
+            presum +=arr[pivot];
+        }
+        return -1;
+    }
+}
